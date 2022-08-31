ce3c83a7c3b8a7d5d8c1b32abb7a6d8771334c19
henriquenicolli <henrique.nicolli@gmail.com>
2022-08-31 17:47:03 -0300

    Criado os filtros

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,21 +4,36 @@ import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
 
-    const options = [
-        {value: 'Henrique', label: 'Henrique'},
-        {value: 'Jose', label: 'Jose'},
-        {value: 'Ana', label: 'Ana'},
+    const months = [
+        {value: 7, label: 'Julho'},
+        {value: 8, label: 'Agosto'},
+        {value: 9, label: 'Setembro'},
+    ];
+
+    const years = [
+        {value: 2022, label: '2022'},
+        {value: 2021, label: '2021'},
+        {value: 2020, label: '2020'},
     ];
 
     return (
         <Container>
             <ContentHeader title="Saidas" lineColor="#E44C4E">
-                <SelectInput options={options}></SelectInput>
+                <SelectInput options={months}></SelectInput>
+                <SelectInput options={years}></SelectInput>
             </ContentHeader>
+
+            <Filters>
+                <button type="button" 
+                className="tag-filter  tag-filter-recurrent">Recorrentes</button>
+
+                <button type="button" 
+                className="tag-filter tag-filter-eventual">Eventuais</button>
+            </Filters>
 
             <Content>
                 <HistoryFinanceCard
